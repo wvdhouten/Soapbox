f434062bfabe91a760c278e8ab5f6376d16843d7
Walter van den Houten <waltervdhouten@gmail.com>
2021-08-30 23:27:11 +0300

    Added default project files

---
+++ Soapbox.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+namespace Soapbox.Web.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
