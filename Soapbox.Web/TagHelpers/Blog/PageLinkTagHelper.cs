2e2a7beb2a1c46796982846aad52327bd29a6647
Walter van den Houten <waltervdhouten@gmail.com>
2021-11-30 16:59:27 +0200

    Add page-link tag helper

---
+++ Soapbox.Web/TagHelpers/Blog/PageLinkTagHelper.cs
@@ -0,0 +1,38 @@
+namespace Soapbox.Web.TagHelpers.Blog
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Microsoft.AspNetCore.Mvc.TagHelpers;
+    using Microsoft.AspNetCore.Mvc.ViewFeatures;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    public class PageLinkTagHelper : TagHelper
+    {
+        private readonly IHtmlGenerator _generator;
+
+        public string Page { get; set; }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public PageLinkTagHelper(IHtmlGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+
+            output.TagName = "a";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            var builder = _generator.GenerateActionLink(ViewContext, string.Empty, Page, "Pages", null, null, null, null, null);
+
+            output.MergeAttributes(builder);
+
+            return Task.CompletedTask;
+        }
+    }
+}
